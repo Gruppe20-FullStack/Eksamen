8bba026a3ac00cae8726b99fe5fa35de1d16cd82
Gruppe20-Fullstack <usn_eksamen@proton.me>
2026-05-04 16:19:21 +0200

    Add domain models for organisations and role persons

---
+++ Models/Organisasjon.cs
@@ -0,0 +1,24 @@
+﻿// Krav 2 - Oppretter klasse Organisasjon:
+using System.ComponentModel.DataAnnotations;
+
+namespace Gruppe20App.Models
+{
+    public class Organisasjon
+    {
+        // Implementerer domener:
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Navn { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(9)]
+        public string Organisasjonsnummer { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        public string? Organisasjonsform {  get; set; }
+
+        public ICollection<RollePerson> RollePersoner { get; set; } = new List<RollePerson>();
+    }
+}
---
+++ Models/RollePerson.cs
@@ -0,0 +1,23 @@
+﻿// Krav 2 - Oppretter klasse RollePerson:
+using System.ComponentModel.DataAnnotations;
+
+namespace Gruppe20App.Models
+{
+    public class RollePerson
+    {
+        // Implementerer domener:
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Navn { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string Rolle { get; set; } = string.Empty;
+
+        public int OrganisasjonId { get; set; }
+
+        public Organisasjon? Organisasjon { get; set; }
+    }
+}
